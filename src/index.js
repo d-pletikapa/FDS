358921f7c348eca294fe3f60076194a79708875e
“Dennys” <d.pletikapa@proton.me>
2023-09-20 23:02:07 +0300

    reupload npm run build

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import auth from "./modules/auth";
-import cart from "./modules/cart";
-import partners from "./modules/partners";
-import slider from "./modules/slider";
-
-auth();
-cart();
-partners();
-slider();
--- src/menu.js
+++
@@ -1,7 +0,0 @@
-import auth from "./modules/auth";
-import cart from "./modules/cart";
-import menu from "./modules/menu";
-
-auth();
-cart();
-menu();
